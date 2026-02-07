5641b2debc5b3cfe4f2fd812e89cdebf979f6fe3
zappybird <150877356+zappybird@users.noreply.github.com>
2026-02-06 23:07:39 -0500

    Update count-loc.sh

--- scripts/count-loc.sh
+++ scripts/count-loc.sh
@@ -1,30 +1,37 @@
 #!/bin/bash
-
 set -e
 
 echo "Reading repos.json..."
 REPOS=$(jq -r '.repos[]' repos.json)
 
-# Create a temp directory
 WORKDIR="loc-temp"
 rm -rf "$WORKDIR"
 mkdir "$WORKDIR"
 
 TOTAL_JSON="{}"
+TOTAL_FILES=0
 
 echo "Cloning repositories and counting LOC..."
 
 for REPO in $REPOS; do
     echo "Processing $REPO..."
 
-    git clone --depth 1 "https://github.com/$REPO.git" "$WORKDIR/$(basename $REPO)"
+    TARGET="$WORKDIR/$(basename $REPO)"
+    git clone --depth 1 "https://github.com/$REPO.git" "$TARGET"
 
-    # Run tokei on the repo
-    REPO_JSON=$(tokei "$WORKDIR/$(basename $REPO)" --output json)
+    # Count files manually
+    FILE_COUNT=$(find "$TARGET" -type f | wc -l)
+    TOTAL_FILES=$((TOTAL_FILES + FILE_COUNT))
 
-    # Merge JSON results
+    # Run tokei
+    REPO_JSON=$(tokei "$TARGET" --output json)
+
+    # Merge JSON
     TOTAL_JSON=$(jq -s '.[0] * .[1]' <(echo "$TOTAL_JSON") <(echo "$REPO_JSON"))
 done
+
+# Inject total file count
+TOTAL_JSON=$(echo "$TOTAL_JSON" | jq --argjson f "$TOTAL_FILES" '.Total.files = $f')
 
 echo "$TOTAL_JSON" > loc-data.json
 echo "LOC data written to loc-data.json"
